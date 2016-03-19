6fd2ba25c3b5425c33ddba01fc88d81515420482
Anthony Ticknor <tony.ticknor@gmail.com>
2016-03-18 22:37:24 -0400

    adding 404 page stub

--- wp-content/themes/apticknor/404.php
+++ wp-content/themes/apticknor/404.php
@@ -8,4 +8,12 @@
 ?>
 <?php get_header(); ?>
 
+<div class="tier">
+    <div class="wrapper">
+        <div class="content" role="main">
+            <h1>Page Not Found</h1>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
