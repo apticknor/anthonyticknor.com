0027e26a9e9f965e5144e308271501004b4faecd
apticknor <tony.ticknor@gmail.com>
2018-03-06 13:41:12 +0000

    new website

---
+++ app/index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en-us">
+    <head>
+        <script>document.documentElement.className += 'js';</script>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>Anthony Ticknor</title>
+        <link rel=stylesheet media="screen and (min-width: 1em)" href="_assets/app.css">
+    </head>
+    <body>
+        <div class="tier">
+            <div class="wrapper wrapper--wide">
+                <div class="hero">
+                    <picture class="isBlock">
+                        <source srcset="_assets/images/hero.jpg" media="(min-width: 600px)"></source>
+                        <source srcset="_assets/images/hero-narrow.jpg"></source>
+                        <img class="isBlock" srcset="_assets/images/hero.jpg" alt="Anthony Ticknor" />
+                    </picture>
+                </div>
+            </div>
+        </div>
+        <div class="tier">
+            <div class="wrapper wrapper--narrow">
+                <main class="main" role="main">
+                    <div class="foreward">
+                        <div class="foreward__hd">
+                            <h1>Hi. I’m Anthony Ticknor, a web designer and VP of Technology at <a href="https://rileyhayes.com/">Riley Hayes</a>.</h1>
+                        </div>
+                        <div class="foreward__bd">
+                            <div class="content">
+                                <p>I believe that that the web shouldn't just look beautiful, it should work beautifully. And that websites should work for people no matter who they are, where they are, or what device is in front of them.</p>
+                                <p>Somewhere along the way I started sharing what I've learned in the last two decades. You can follow along with me on <a href="https://github.com/apticknor">Github</a>, <a href="https://twitter.com/apticknor">Twitter</a>, <a href="https://dribbble.com/apticknor">Dribbble</a>, and <a href="https://codepen.io/apticknor/">CodePen</a>.</p>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+        <div class="tier">
+            <div class="wrapper">
+
+            </div>
+        </div>
+
+        <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "Person",
+            "givenName": "Anthony",
+            "familyName": "Ticknor",
+            "name": "Anthony Ticknor",
+            "birthdate": "1979-11-05",
+            "homeLocation": {
+                "@type": "Place",
+                "address": {
+                    "@type": "PostalAddress",
+                    "addressLocality": "Minneapolis",
+                    "addressRegion": "MN"
+                }
+            },
+            "affiliation": {
+                "@type": "Organization",
+                "name": "Riley Hayes Advertising"
+            },
+            "jobTitle": "Vice President of Technology",
+            "image": "http://www.anthonyticknor.com/_assets/images/me.jpg",
+            "url": "http://www.anthonyticknor.com"
+        }
+        </script>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+        <script src="_assets/app.js"></script>
+    </body>
+</html>
