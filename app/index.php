f206b22903f42d155fbd0ba85957b6478d4b4645
EC2 Default User <ec2-user@ip-172-31-16-218.us-west-2.compute.internal>
2019-06-29 19:19:15 +0000

    removing totally unecessary javascript

--- app/index.php
+++ app/index.php
@@ -68,8 +68,10 @@
             "url": "http://www.anthonyticknor.com"
         }
         </script>
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-        <script src="_assets/app.js"></script>
+        
+        <?php
+            // <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+            // <script src="_assets/app.js"></script>
+        ?>
     </body>
 </html>
